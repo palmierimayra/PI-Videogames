3c1f98264e447b770785a222dcf5a4f2b99d6f43
Mayra <palmierimayra@gmail.com>
2024-11-09 18:47:32 -0300

    prueba

--- api/src/app.js
+++ api/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
@@ -7,39 +8,23 @@ const routes = require('./routes/index.js');
 require('./db.js');
 
 const server = express();
-
 server.name = 'API';
+
+// Configuración de CORS
+server.use(cors({
+  origin: 'https://palmierimayra-videogames.vercel.app',
+  credentials: true,
+}));
 
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use((req, res, next) => {
-  const allowedOrigins = [
-    'https://pi-videogames-production-21fb.up.railway.app',
-    'https://palmierimayra-videogames.vercel.app'
-  ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  }
-
-  // Si la solicitud es de tipo OPTIONS, responde con status 200
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  
-  next();
-});
-
 
 server.use('/', routes);
 
-// Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+// Manejo de errores
+server.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
